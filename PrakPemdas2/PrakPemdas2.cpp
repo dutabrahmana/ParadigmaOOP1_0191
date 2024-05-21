d22967f92ba4a573d95af25c5eb10ef9bb39c99c
dutabrahmana <dutalinggau25@gmail.com>
2024-05-21 11:59:05 +0700

    menambahkan delete

--- PrakPemdas2/PrakPemdas2.cpp
+++ PrakPemdas2/PrakPemdas2.cpp
@@ -16,6 +16,19 @@ int main()
 	varIbu->tambahAnak(varAnak1);
 	varIbu->tambahAnak(varAnak2);
 	varIbu->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	
+
 
 	return 0;
 }
