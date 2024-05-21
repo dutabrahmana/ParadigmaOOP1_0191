c70f2d450333dcb676ec01549557fac9fdf144a8
dutabrahmana <dutalinggau25@gmail.com>
2024-05-21 11:34:02 +0700

    menambahkan isi int main

--- PrakPemdas2/PrakPemdas2.cpp
+++ PrakPemdas2/PrakPemdas2.cpp
@@ -7,5 +7,9 @@ using namespace std;
 
 int main()
 {
-    
+    ibu* variIbu = new ibu("dini");
+    ibu* variIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
 }
